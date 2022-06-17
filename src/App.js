b843a2b0415d831dfbf017275e5f8d7bd38317c7
Marcoscappelli <cappelli.m@gmail.com>
2022-06-17 20:16:59 -0300

    formulário criação projeto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 //Pages
-import { BrowserRouter as Router, Routes, Route,} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
 import Projects from './components/pages/Projects';
+import Project from './components/pages/Project';
 //Components
 import Container from './components/layout/Container';
 import Navbar from './components/layout/Navbar';
@@ -18,11 +19,12 @@ function App() {
          <Container customClass="min-height">
 
         <Routes>
-          <Route exact path="/" element={<Home/>}/>
+          <Route path="/" element={<Home/>}/>
           <Route path="/projects" element={<Projects/>}/>
           <Route path="/company" element={<Company/>}/>
           <Route path="/contact" element={<Contact/>}/>
           <Route path="/newproject" element={<NewProject/>}/>
+          <Route path='/project/:id' element={<Project/>}/>
         </Routes>
 
          </Container>
