0009d021299eaa22da3f53c9ac99ea10faf29b5a
Marcoscappelli <cappelli.m@gmail.com>
2022-06-20 21:16:42 -0300

    ServiceCard.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
 import Projects from './components/pages/Projects';
+import Project from './components/pages/Project';
 
 //Components
 import Container from './components/layout/Container';
@@ -24,6 +25,7 @@ function App() {
           <Route path="/company" element={<Company/>}/>
           <Route path="/contact" element={<Contact/>}/>
           <Route path="/newproject" element={<NewProject/>}/>
+          <Route path="/project/:id" element={<Project />}/>
         </Routes>
 
          </Container>
